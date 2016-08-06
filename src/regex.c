22c1c39cdd1bd8d89377bc2a7f206480831f6163
Todd Lewis <utoddl@email.unc.edu>
2016-08-05 20:22:07 -0400

    Backward FindRegExp works again.

--- src/regex.c
+++ src/regex.c
@@ -1,24 +1,25 @@
 /* Extended regular expression matching and search library.
-   Copyright (C) 2002-2015 Free Software Foundation, Inc.
+   Copyright (C) 2002-2016 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Isamu Hasegawa <isamu@yamato.ibm.com>.
 
    The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public
+   modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
-   version 3 of the License, or (at your option) any later version.
+   version 2.1 of the License, or (at your option) any later version.
 
    The GNU C Library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
+   Lesser General Public License for more details.
 
-   You should have received a copy of the GNU General Public
+   You should have received a copy of the GNU Lesser General Public
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
 #ifndef _LIBC
 /* PORTABILITY PROBLEM: We do not include config.h. */
+/* # include <config.h>*/
 
 # if (__GNUC__ == 4 && 6 <= __GNUC_MINOR__) || 4 < __GNUC__
 #  pragma GCC diagnostic ignored "-Wsuggest-attribute=pure"
@@ -65,11 +66,8 @@
 #include <limits.h>
 
 /* PORTABILITY PROBLEM: "regex.h" instead of <regex.h>; bool type injected. */
-typedef int bool;
-#define false (0)
-#define true (1)
+/* #include <regex.h> */
 #include "regex.h"
-
 #include "regex_internal.h"
 
 #include "regex_internal.c"
