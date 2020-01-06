3be52cb26cdcffadaed3e757e23d6d11a36c6e6a
Todd Lewis <utoddl@email.unc.edu>
2020-01-06 08:09:05 -0500

    bump copyrights to 2020

--- src/autocomp.c
+++ src/autocomp.c
@@ -1,6 +1,6 @@
 /* AutoComplete
 
-   Copyright (C) 2010-2019 Todd M. Lewis and Sebastiano Vigna
+   Copyright (C) 2010-2020 Todd M. Lewis and Sebastiano Vigna
 
    This file is part of ne, the nice editor.
 
